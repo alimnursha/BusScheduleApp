9470be8521af2fe24d23474e9a7fb57fb1f05721
Alimkhan Nursha <200101084@stu.sdu.edu.kz>
2023-12-26 12:58:01 +0600

    Create a Room entity

--- app/src/main/java/com/example/busschedule/data/BusSchedule.kt
+++ app/src/main/java/com/example/busschedule/data/BusSchedule.kt
@@ -15,8 +15,16 @@
  */
 package com.example.busschedule.data
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "schedule")
 data class BusSchedule(
+    @PrimaryKey
     val id: Int,
-    val stopName: String,
-    val arrivalTimeInMillis: Int
+    @ColumnInfo(name = "stop_name")
+    val stopName: String?,
+    @ColumnInfo(name = "arrival_time")
+    val arrivalTimeInMillis: Int?
 )
